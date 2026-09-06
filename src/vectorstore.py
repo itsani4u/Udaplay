c7535576197af59be80199ebcdc759ad67ee84af
itsani4u <36327045+itsani4u@users.noreply.github.com>
2026-09-06 12:10:15 +0530

    Update vectorstore.py

--- src/vectorstore.py
+++ src/vectorstore.py
@@ -1,7 +1,7 @@
 import os
 import json
 from langchain_community.vectorstores import FAISS
-from langchain.schema import Document
+from langchain_core.documents import Document
 from src.embeddings import get_gemini_embeddings
 from src.logger import logger
 
